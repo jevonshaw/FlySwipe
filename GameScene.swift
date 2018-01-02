1a6d57d567f14dce32602fed440d2279c6f7e16a
jevonshaw <jevoneshaw@yahoo.com>
2018-01-02 14:10:16 -0600

    Update GameScene.swift

--- GameScene.swift
+++ GameScene.swift
@@ -10,67 +10,75 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var xreach: CGFloat = 250
+    var yreach: CGFloat = 480
+    var boundary = SKSpriteNode()
     var paw = SKSpriteNode()
-    //var fly = SKSpriteNode()
+    var gameTimer:Timer!
+    
+    //game timer? font size, font name, font color, pythagorean thm to calc distance.
     
     override func didMove(to view: SKView) {
+        /*
+        boundary = self.childNode(withName: "boundary") as! SKSpriteNode
+        boundary.position = CGPoint(x: 0.0, y: 0.0)
+ */
         paw = self.childNode(withName: "paw") as! SKSpriteNode
-        //fly = self.childNode(withName: "fly") as! SKSpriteNode
+        paw.position = CGPoint(x: 0.0, y: -480)
+        paw.anchorPoint = CGPoint(x: 0.5, y: 0.8)
+        paw.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: paw.size.width, height: paw.size.height/3))
+        paw.physicsBody?.affectedByGravity = false
+        paw.physicsBody?.linearDamping = 15
+        let orConstraint = SKConstraint.orient(to: CGPoint(x: 0.0, y: 0.0), offset: SKRange(constantValue: (3 * CGFloat.pi / 2)))
+        paw.constraints = [orConstraint]
+        gameTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(addFly), userInfo: nil, repeats: true)
 
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-
+    @objc func addFly() {
+        
+        //initialize fly parameters
+        let fly = SKSpriteNode(imageNamed: "fly")
+        let randomPos = GKRandomDistribution(lowestValue: -320, highestValue: 320)
+        let flyPos = CGPoint(x: randomPos.nextInt(), y: -200)
+        fly.physicsBody = SKPhysicsBody(rectangleOf: fly.size)
+        fly.physicsBody?.isDynamic = true
+        fly.physicsBody?.friction = 0
+        fly.physicsBody?.affectedByGravity = false
+        fly.position = flyPos
+        self.addChild(fly)
+        
+        //fly velocity setter
+        func setFlyVel(delx: CGFloat, dely: CGFloat){
+            fly.physicsBody?.velocity = CGVector(dx: delx, dy: dely)
+        }
+        setFlyVel(delx: -200.0, dely: 462.0)
+        
+        //if the fly goes off the screen, delete it
+        if abs(fly.position.x) > 400 || abs(fly.position.y) > 650{
+            fly.removeFromParent()
+        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
 
     }
     
-    func rotatePawBy(thisx: CGFloat, thisy: CGFloat){
-        paw.zRotation = atan(thisy / thisx)
-        
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         for touch in touches {
-            // add if statement to tell if the touch is outside of a certain boundary, then stay in the boundary,
-            // but still rotate and move.
             let location = touch.location(in: self)
-            paw.run(SKAction.moveTo(x: location.x, duration: 0))
+            paw.run(SKAction.moveTo(x: location.x, duration: 0)) // move to whatever point is touched
             paw.run(SKAction.moveTo(y: location.y, duration: 0))
-            rotatePawBy(thisx: location.x, thisy: location.y)
-            //fix the code below. it may be wrong
-           /* if abs(location.y) < 480 {
-                location.y = 480
-            } else {
-                
-            }*/
-
         }
-
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            // add if statement to tell if the touch is outside of a certain boundary, then stay in the boundary,
-            // but still rotate and move
             let location = touch.location(in: self)
-            paw.run(SKAction.moveTo(x: location.x, duration: 0))
+            paw.run(SKAction.moveTo(x: location.x, duration: 0)) // move to wherever the touch is moved to
             paw.run(SKAction.moveTo(y: location.y, duration: 0))
-            rotatePawBy(thisx: location.x, thisy: location.y)
-            //fix the code below. it may be wrong
-            //if abs(location.y) < 480 {
-                //location.y = 480
-            //}
         }
-
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -82,5 +90,6 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+
     }
 }
